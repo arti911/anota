cb594b2fa9591554e1bbd20b5d1dacc058e41b1e
Artur Papikian <34072449+arti911@users.noreply.github.com>
2021-08-30 17:27:23 +0300

    added search

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,11 @@
-import { Row, Col, Layout, Statistic } from "antd";
+import { SyntheticEvent, useCallback } from "react";
+import { Layout, Statistic, Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
 
-import { useAppSelector } from "../../hook";
+import { useAppDispatch, useAppSelector } from "../../hook";
+
+import { setSearch } from "../../appSlice";
+import { searchNotesFn } from "../../utils";
 
 import Logo from "../Logo";
 
@@ -9,21 +14,41 @@ import "./style.scss";
 const { Header } = Layout;
 
 const HeaderAnota = () => {
+  const dispatch = useAppDispatch();
   const notes = useAppSelector((state) => state.anota.notes);
+  const search = useAppSelector((state) => state.anota.search);
+
+  const onSearch = useCallback((event: SyntheticEvent) => {
+    const element = event.target as HTMLInputElement;
+
+    const searchNotes = searchNotesFn(notes, element.value);
+
+    dispatch(setSearch({
+      notes: element.value === "" ? [] : searchNotes,
+      value: element.value
+    }));
+  }, [ dispatch, notes ]);
+
+  const countNotes = search.notes.length > 0
+    ? search.notes.length
+    : search.value !== "" && search.notes.length === 0
+    ? search.notes.length
+    : notes.length;
 
   return (
     <Header className="anota-header">
-      <Row justify="space-between" align="middle">
-        <Col>
-          <Logo />
-        </Col>
-        <Col>
-          <Statistic
-            title="Количество заметок"
-            value={notes.length}
-          />
-        </Col>
-      </Row>
+      <div className="anota-header__item anota-header__logo">
+        <Logo />
+      </div>
+      <div className="anota-header__item anota-header__search">
+        <Input size="large" placeholder="search" prefix={<SearchOutlined />} allowClear onChange={onSearch} />
+      </div>
+      <div className="anota-header__item anota-header__count">
+        <Statistic
+          title="Количество заметок"
+          value={countNotes}
+        />
+      </div>
     </Header>
   );
 };
