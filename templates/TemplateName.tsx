c8d4cdf0bb531a1ab11eaecd584ed8e28f69c869
Artur Papikian <34072449+arti911@users.noreply.github.com>
2023-01-20 14:18:38 +0300

    added templates

---
+++ templates/TemplateName.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.scss";
+
+const TemplateName = () => {
+  return (
+    <div className="template-name" data-testid="TemplateName">
+      <h1>TemplateName component</h1>
+    </div>
+  );
+};
+
+export default TemplateName;
