7c1b7e0c19f4bdb65103719a6e6e5093638d291d
Artur Papikian <34072449+arti911@users.noreply.github.com>
2023-03-22 13:59:56 +0300

    updated storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,3 @@
-import { Provider } from "react-redux";
-
-import { store } from "../src/store";
-
-import "../src/styles/index.scss";
-import "antd/dist/antd.css";
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -14,11 +7,3 @@ export const parameters = {
     },
   },
 }
-
-export const decorators = [
-  (Story) => (
-    <Provider store={store}>
-      <Story />
-    </Provider>
-  )
-]
