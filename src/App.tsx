8217a12bfd4f8f4999e3d6daa5c76c14e27d4ce2
APapikian <hafud@mail.ru>
2026-03-25 15:53:21 +0300

    added separator

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ const App = () => (
     <h1>Hello, World..</h1>
     <hr />
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur massa nulla, vehicula varius malesuada sed, gravida eget risus. Duis rutrum dictum pretium. Pellentesque semper malesuada urna. Phasellus eleifend facilisis mauris, et feugiat magna volutpat ut. Vivamus ac rhoncus ipsum, ut feugiat lorem. Aenean vel bibendum metus. Aliquam a libero ut nibh auctor viverra in sed urna. Ut eget volutpat lectus. Vestibulum fermentum rhoncus justo, quis venenatis augue maximus at. Integer id augue sed urna porttitor mollis. Vestibulum lobortis turpis eget erat pulvinar ullamcorper.</p>
+    <hr />
     {/* <HeaderAnota />
     <ContentAnota />
     <FooterAnota />
